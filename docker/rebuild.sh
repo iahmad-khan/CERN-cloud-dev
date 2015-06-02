7982868ddacc437934b6a8447040043a95925d49
Ricardo Rocha <rocha.porto@gmail.com>
2015-06-02 15:29:21 +0200

    yum.conf yum-puppet.repos.d config moved to puppetbase.
    also changed rebuild.sh to use a fixed set of docker dirs.

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-for dir in *; do
-	if [ -d ${dir} ] && [ -e ${dir}/Dockerfile ]; then
-		sudo docker build -t rochaporto/${dir} ${dir}
-	fi
+for dir in base puppetbase puppetdb puppetagent puppetmaster; do
+	sudo docker build -t rochaporto/${dir} ${dir}
 done
