ef6a73b03a611b4855daec359d7f4503d84a97c1
Ricardo Rocha <rocha.porto@gmail.com>
2015-06-03 16:26:31 +0200

    added teigi to list of docker builds.
    plus some extra debugging output.

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+if [ "$1" != "push" ]; then
+	echo "REBUILD: Building locally only... pass 'push' as arg to also push to registry"
+else
+	echo "REBUILD: Will push images to registry"
+fi
 
-for dir in base puppetbase puppetdb puppetagent puppetmaster; do
-	sudo docker build -t rochaporto/${dir} ${dir}
+for dir in base puppetbase puppetdb puppetagent puppetmaster ../teigi; do
+	img=$(echo ${dir} | cut -d'/' -f 2)
+	echo "REBUILD: Building image ${img}"
+	sudo docker build -t rochaporto/${img} ${dir}
+	if [ $? != 0 ]; then
+		echo "REBUILD: Docker build of ${dir} failed"
+		exit $?
+	fi
+	if [ "$1" == "push" ]; then
+		sudo docker push rochaporto/${img}
+	fi
 done
