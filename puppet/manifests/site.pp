cbd66614101641b016b0f4f8ff5ba713f60acf92
Ricardo Rocha <rocha.porto@gmail.com>
2015-07-16 18:14:29 +0200

    1719: add glance container (in controller pod)
    adds a new glance pod with a container using ceph as store.
    
    Fixes #OS-1719.

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -5,3 +5,8 @@ node /.*keystone.*/ {
   class {'hg_cloud_identity': }
   class {'hg_cloud_identity::backend': }
 }
+
+node /.*glance.*/ {
+  class {'hg_cloud_image': }
+  class {'hg_cloud_image::worker': }
+}
