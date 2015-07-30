947a9b15f9dbd6cfa90f94bce47fcbb26dd95c0c
Ricardo Rocha <rocha.porto@gmail.com>
2015-07-30 16:10:26 +0200

    use kubectl, added timeouts to ceph cmds.
    use kubectl instead of kubectl.sh, the binary gets places in the _output
    directory after the build.

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 for z in dns-hack.json skydns-rc.yaml skydns-svc.yaml puppet-pod.yaml puppet-svc.yaml controller-pod.yaml controller-svc.yaml ceph-pod.yaml; do
-	kubectl.sh create -f $z
+	kubectl create -f $z
 done
 
-ceph auth add client.images -i ../ceph/ceph.client.images.keyring
-ceph auth add client.volumes -i ../ceph/ceph.client.volumes.keyring
-ceph osd pool create images 64
-ceph osd pool create volumes 64
+kubectl get pod ceph | grep Pending > /dev/null 2>&1
+while [ $? -eq 0 ]
+do
+  echo "waiting for ceph pod to be ready..."
+  sleep 2
+  kubectl get pod ceph | grep Pending > /dev/null 2>&1
+done
+
+kubectl exec -it -p ceph -c cephall -- /usr/bin/ceph --connect-timeout 10 auth add client.images -i ../ceph/ceph.client.images.keyring
+kubectl exec -it -p ceph -c cephall -- /usr/bin/ceph --connect-timeout 10 auth add client.volumes -i ../ceph/ceph.client.volumes.keyring
+kubectl exec -it -p ceph -c cephall -- /usr/bin/ceph --connect-timeout 10 osd pool create images 64
+kubectl exec -it -p ceph -c cephall -- /usr/bin/ceph --connect-timeout 10 osd pool create volumes 64
