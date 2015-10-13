94bb6b4f6ce78b4da77a35c10d5d46e0b546741d
Ricardo Rocha <rocha.porto@gmail.com>
2015-10-13 11:23:19 +0200

    add missing nova/compute patches.
    add the missing patches on nova and compute nodes to get the dev
    environment working with the cern packages.

--- puppet/manifests/nodes/nova.pp
+++ puppet/manifests/nodes/nova.pp
@@ -78,11 +78,31 @@ node /.*nova.*/ inherits default {
   exec { '/usr/bin/patch -p0 /usr/lib/python2.7/site-packages/nova/cern.py < /tmp/patch':
     unless      => "/usr/bin/grep puppet /usr/lib/python2.7/site-packages/nova/cern.py",
   }
-  ~>
+  ->
+  package { 'python-landbclient':
+    ensure => present,
+  }
+  ->
+  exec { '/usr/bin/landb --landb-host puppet --landb-port 8443 --landb-user someuser --landb-password 111 ipsrv-register CLUSTER1 compute':
+    unless => '/usr/bin/landb --landb-host puppet --landb-port 8443 --landb-user someuser --landb-password 111 cluster-dev CLUSTER1 | grep Z',
+  }
+  ->
+  exec { "/usr/bin/sed -i 's/\.CERN\.CH//g' /usr/lib/python2.7/site-packages/nova/network/manager.py":
+    onlyif => "/usr/bin/grep 'CERN.CH' /usr/lib/python2.7/site-packages/nova/network/manager.py",
+  }
+  ->
+  exec { "/usr/bin/mysql -u nova -D nova -p123456 -h controller -e \"insert into networks (injected, cidr, bridge, gateway, dns1, cidr_v6, gateway_v6, label, multi_host, dns2, uuid, deleted, enable_dhcp, share_address) values (0, '0.0.0.0/0', 'br100', '0.0.0.0', '0.0.0.0', '::/0', '::', 'KUB_NETWORK', 0, '0.0.0.0', '81b1390e-6d0e-11e5-87fb-68f728b18b2d', 0, 1, 0);\"":
+    unless => "/usr/bin/mysql -u nova -D nova -p123456 -h controller -e \"select * from networks where label = 'KUB_NETWORK';\" | grep KUB",
+  }
+  ->
+  exec { '/usr/bin/nova-manage cern network_update --cluster-name CLUSTER1':
+    unless => "/usr/bin/mysql -u nova -D nova -p123456 -h controller -e \"select * from fixed_ips where address = '10.0.0.200';\" | grep 200",
+  }
+  ->
   Service['openstack-nova-scheduler']
-  ~>
+  ->
   Service['openstack-nova-conductor']
-  ~>
+  ->
   Service['openstack-nova-api']
 
 }
