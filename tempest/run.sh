9cc2ce86b7216c7a33fa53545f65fe56fb6bbb34
Ricardo Rocha <rocha.porto@gmail.com>
2015-12-02 17:11:12 +0100

    fetch colorizer.py for tempest runs.
    seems it was removed from the upstream code, keep it for now as a
    workaround.

--- tempest/run.sh
+++ tempest/run.sh
@@ -11,6 +11,7 @@ cd /tempest
 if [[ ! -d .venv ]]; then
 	python tools/install_venv.py --no-site-packages
 fi
+wget https://raw.githubusercontent.com/openstack/tempest/8843f0f0768019c76be72b4be2f6a156cdbe3d78/tools/colorizer.py -O tools/colorizer.py
 testr init > /dev/null 2>&1 || true
 ./tools/with_venv.sh testr run --subunit --load-list /etc/tempest/tempest.list | subunit-2to1 | tools/colorizer.py
 exit $?
