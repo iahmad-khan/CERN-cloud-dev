24a6bc8e92b671ed2af9c69c59a298978f2b3dea
Bertrand NOEL <bertrand.noel@cern.ch>
2016-01-28 13:44:03 +0100

    Put modules and hostgroups one per line. It's way better for Git commit changes reviews

--- scripts/cci-dev.sh
+++ scripts/cci-dev.sh
@@ -15,10 +15,63 @@ fi
 export PATH=$PATH:$CLOUDDEV_KUB/_output/local/go/bin
 
 # PUPPET_MODULES holds the list of module dependencies that we need to run the build
-PUPPET_MODULES="abrt afs apache bridged cernlib cinder cloud_common:optional-abrt-psacct cloud_monitoring concat filemapper firewall flume glance haproxy horizon inifile kerberos keystone lemon limits logrotate magnum memcached motd mysql network neutron:1748-neutrondev nova openstack_clients openstacklib:kilo osrepos psacct puppet puppetdbquery sssd stdlib sudo swap_file sysctl teigi:tbag_teigiurl xinetd"
+PUPPET_MODULES="
+abrt
+afs
+apache
+bridged
+cernlib
+cinder
+cloud_common:optional-abrt-psacct
+cloud_monitoring
+concat
+filemapper
+firewall
+flume
+glance
+haproxy
+horizon
+inifile
+kerberos
+keystone
+lemon
+limits
+logrotate
+magnum
+memcached
+motd
+mysql
+network
+neutron:1748-neutrondev
+nova
+openstack_clients
+openstacklib:kilo
+osrepos
+psacct
+puppet
+puppetdbquery
+sssd
+stdlib
+sudo
+swap_file
+sysctl
+teigi:tbag_teigiurl
+xinetd
+"
 
 # PUPPET_HOSTGROUPS holds the list of hostgroups we need to run the build(s)
-PUPPET_HOSTGROUPS="cloud_adm:qa cloud_blockstorage cloud_compute:devenv cloud_container cloud_dashboard cloud_identity cloud_image:swap cloud_networking:containerdev cloud_orchestration cloud_telemetry"
+PUPPET_HOSTGROUPS="
+cloud_adm:qa
+cloud_blockstorage
+cloud_compute:devenv
+cloud_container
+cloud_dashboard
+cloud_identity
+cloud_image:swap
+cloud_networking:containerdev
+cloud_orchestration
+cloud_telemetry
+"
 
 # OS_PODS holds the list of pods to be started on 'launch'
 OS_PODS=${OS_PODS:-keystone glance cinder neutron nova compute client horizon}
