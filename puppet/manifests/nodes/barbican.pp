890165c69b7173c01350b9bd4a4fe15b91cff18a
Ricardo Rocha <rocha.porto@gmail.com>
2016-04-20 09:22:04 +0200

    fix puppet order in barbican node.

--- puppet/manifests/nodes/barbican.pp
+++ puppet/manifests/nodes/barbican.pp
@@ -9,15 +9,13 @@ node /.*barbican.*/ inherits default {
     ensure => 'present',
   }
   ->
-  Package['openstack-barbican-api']
-  ->
-  Teigi::Secret<||>
-  ->
   exec {'create-barbican-db':
     command     => "/usr/bin/mysql -u root -h controller -p123456 -e \"create database barbican CHARACTER SET utf8 COLLATE utf8_general_ci; grant all privileges on barbican.* to 'barbican'@'%' identified by '123456';\"",
     environment => 'TERM=xterm',
     unless      => "/usr/bin/mysql -u root -h controller -p123456 -e \"use barbican\"",
   }
+  ->
+  Package['openstack-barbican-api']
   ->
   exec { 'update-barbican-db':
     command     => "/usr/bin/barbican-db-manage -d 'mysql://barbican:123456@controller:/barbican' upgrade",
