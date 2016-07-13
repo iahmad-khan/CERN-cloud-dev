aa114c1d21496b33ea3b69d27d1c0fb289e5770c
Jose Castro Leon <jose.castro.leon@cern.ch>
2016-07-13 13:42:26 +0200

    Fix typo on create mistral database

--- puppet/manifests/nodes/mistral.pp
+++ puppet/manifests/nodes/mistral.pp
@@ -8,7 +8,7 @@ node /.*mistral.*/ inherits default {
     ensure => 'present',
   }
   ~>
-  exec {'create-cinder-db':
+  exec {'create-mistral-db':
     command     => "/usr/bin/mysql -u root -h controller -p123456 -e \"create database mistral CHARACTER SET utf8 COLLATE utf8_general_ci; grant all privileges on mistral.* to 'mistral'@'%' identified by '123456';\" || true",
     environment => 'TERM=xterm',
     refreshonly => true,
